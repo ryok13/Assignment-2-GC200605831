8016b01ce146616ab99ec3cf0aeceb111958d49b
ryok13 <mario24ryok@gmail.com>
2025-07-19 20:49:22 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.georgiancollege.assignment2gc200605831 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.georgiancollege.assignment2gc200605831 to javafx.fxml;
+    exports com.georgiancollege.assignment2gc200605831;
+}
