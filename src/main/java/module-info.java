4bc141a7fb4dcaaa7f0bbd0dc7c14731c5b50850
ryok13 <mario24ryok@gmail.com>
2025-08-03 14:09:39 -0400

    Add Gson library, and update project/module configuration files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.georgiancollege.assignment2gc200605831 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires java.desktop;
+    requires com.google.gson;
 
 
-    opens com.georgiancollege.assignment2gc200605831 to javafx.fxml;
+    opens com.georgiancollege.assignment2gc200605831 to javafx.fxml, com.google.gson;
     exports com.georgiancollege.assignment2gc200605831;
 }
